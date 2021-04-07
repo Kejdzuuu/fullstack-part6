4b210def4d29e0dcb151cdeb8297aa4072a8335c
Muciej <kejdzuuu@gmail.com>
2021-04-07 18:05:02 +0200

    Ex. 6.2

--- src/index.js
+++ src/index.js
@@ -6,21 +6,39 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const goodClickHandler = () => {
     store.dispatch({
       type: 'GOOD'
     })
   }
 
+  const okClickHandler = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const badClickHandler = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const resetClickHandler = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={goodClickHandler}>good</button>
+      <button onClick={okClickHandler}>neutral</button>
+      <button onClick={badClickHandler}>bad</button>
+      <button onClick={resetClickHandler}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
